3b939d60767807e1b30068f5aaef1d47662f050e
Caleb Abbruzzese <caleb@openarc.net>
2015-03-11 12:24:35 -0400

    Added html pages for testing. Added jquery ajax call for retrieving page contents. Added canned code for retrieving query strings. Currently failing on a Cross-Domain error, needs to be moved to PHP.

--- scripts/replace.js
+++ scripts/replace.js
@@ -1,12 +1,24 @@
-if (!window.jQuery) {
+/*if (!window.jQuery) {
   var jq = document.createElement('script'); jq.type = 'text/javascript';
   // Path to jquery.js file, eg. Google hosted version
   jq.src = '/path-to-your/jquery.min.js';
   document.getElementsByTagName('head')[0].appendChild(jq);
+}*/
+
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 
+var urlString = getParameterByName("url");
+
 setTimeout(function(){ 
-	var taghtml = '<img style="position: absolute; left: 200px; top: 200px;" src="http://bacolicious.s3.amazonaws.com/bacon.png">';
-	$('body').append(taghtml);
+
+	$('body').load( urlString, null, new function () {		
+		var taghtml = '<img style="position: absolute; left: 200px; top: 200px; width: 80%;" src="http://bacolicious.s3.amazonaws.com/bacon.png">';
+		$('body').append(taghtml);
+	});
 	
-}, 1000);
+}, 300);
