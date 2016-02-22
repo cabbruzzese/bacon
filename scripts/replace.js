53f23fe1124ac1bd17d2f18b802ecdc3ac4e8bc1
Caleb Abbruzzese <caleb@openarc.net>
2016-02-22 09:41:20 -0500

    old stuff

--- scripts/replace.js
+++ scripts/replace.js
@@ -1,7 +1,7 @@
 /*if (!window.jQuery) {
   var jq = document.createElement('script'); jq.type = 'text/javascript';
   // Path to jquery.js file, eg. Google hosted version
-  jq.src = '/path-to-your/jquery.min.js';
+  jq.src = 'https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js';
   document.getElementsByTagName('head')[0].appendChild(jq);
 }*/
 
